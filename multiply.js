ff9556f467ae201e8fc6191938b2f408d02978ac
grentank <mexmat100394@gmail.com>
2022-08-11 17:54:28 +0300

    super pormise

--- multiply.js
+++ multiply.js
@@ -1,5 +1,8 @@
-function multiply() {
-
+function multiply(arg1, ...arr) {
+  const a = new Promise((res, rej) => {
+    res(5);
+  });
+  return a.then((data) => data - 2);
 }
 
 module.exports = multiply;
